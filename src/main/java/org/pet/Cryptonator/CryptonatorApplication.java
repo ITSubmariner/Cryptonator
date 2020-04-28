b9afccf7ee78e22080084cd70844d43b2a0cdb6c
Антон <it.novikov.anton@yandex.ru>
2020-04-28 20:19:22 +0300

    init

---
+++ src/main/java/org/pet/Cryptonator/CryptonatorApplication.java
@@ -0,0 +1,11 @@
+package org.pet.Cryptonator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptonatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CryptonatorApplication.class, args);
+    }
+}
