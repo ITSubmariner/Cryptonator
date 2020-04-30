b5f924b50368d8e3dd81807970a131d4e9256078
Антон <it.novikov.anton@yandex.ru>
2020-04-30 22:25:38 +0300

    added webpack+react, mvc config

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        main: path.join(__dirname, 'src', 'main', 'resources', 'js', 'main.js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src', 'main', 'resources', 'js'),
+            path.join(__dirname, 'node_modules'),
+        ],
+    }
+}
